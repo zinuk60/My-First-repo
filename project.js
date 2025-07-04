c1fc12166490573e0b33603cb196dd1e6b8c6c9b
zinuk60 <zinukdebnath6010@gmail.com>
2025-07-04 16:27:45 +0600

    my love language

---
+++ project.js
@@ -0,0 +1,22 @@
+const dd=document.querySelector('.dd');
+const ff =document.querySelector('.ff');
+const qq =document.querySelector('.qq');
+const zq =document.querySelector('.zq');
+const ddRect = dd.getBoundingClientRect();
+const zqReact =zq.getBoundingClientRect();
+qq.addEventListener('click',() => {
+    ff.innerText = 'love you pronomita';
+    });
+zq.addEventListener('mouseover' , ()=> {
+    const i = Math.floor(Math.random() * 
+    (ddRect.width - zqReact.width))
+    +1;
+    const j = Math.floor(Math.random() *
+    (ddRect.height - zqReact.height)) + 1 ;
+    zq.style.left = i + 'px';
+    zq.style.top = j + 'px';
+});
+
+
+
+
